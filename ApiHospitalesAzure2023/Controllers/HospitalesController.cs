9b9219572a519f9ccb9935da162c065765930fce
Evelyn Cuenca <eve.cc@outlook.es>
2023-01-30 16:37:20 +0000

    Add project files.

---
+++ ApiHospitalesAzure2023/Controllers/HospitalesController.cs
@@ -0,0 +1,33 @@
+﻿using ApiHospitalesAzure2023.Models;
+using ApiHospitalesAzure2023.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiHospitalesAzure2023.Controllers
+{
+    //https://servicioapo.com/api/hospitales
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HospitalesController : ControllerBase
+    {
+        private RepositoryHospital repo;
+        public HospitalesController(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+
+        //https://servicioapo.com/api/hospitales
+        [HttpGet]
+        public ActionResult<List<Hospital>> GetHospitales()
+        {
+            return this.repo.GetHospitales();
+        }
+
+        //https://servicioapo.com/api/hospitales
+        [HttpGet("{id}")]
+        public ActionResult<Hospital> FindHospital(int id)
+        {
+            return this.repo.FindHospital(id);
+        }
+    }
+}
